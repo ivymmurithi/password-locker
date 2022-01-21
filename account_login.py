30bfe88c6fa2cb7aa99f6ddc419ad504bf4cca95
ivymmurithi <ivymurithi@gmail.com>
2022-01-21 21:24:55 +0300

    add an empty list for storing usernames and passwords

--- account_login.py
+++ account_login.py
@@ -3,6 +3,11 @@ class User:
     A class that generates new instances of user accounts created
     """
 
+    user_logins = []
+    """
+    An empty list to store the user_name and password
+    """
+
     def __init__(self,user_name,password):
         """
         This method will create new instances of the User class.
