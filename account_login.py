e6d803e99585adb974ac9264685a32d908b5f0ef
ivymmurithi <ivymurithi@gmail.com>
2022-01-21 21:20:39 +0300

    create new instances of User class

--- account_login.py
+++ account_login.py
@@ -2,3 +2,15 @@ class User:
     """
     A class that generates new instances of user accounts created
     """
+
+    def __init__(self,user_name,password):
+        """
+        This method will create new instances of the User class.
+
+        Args:
+        user_name: New User username.
+        password: New password for password.
+        """
+
+        self.user_name =  user_name
+        self.password = password
