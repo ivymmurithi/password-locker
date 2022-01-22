5f29b67b9583999317543d438ad84a38cff4da5c
ivymmurithi <ivymurithi@gmail.com>
2022-01-22 21:29:40 +0300

    add save and delete methods

--- run.py
+++ run.py
@@ -2,5 +2,21 @@
 
 from account_login import User
 
+def new_logins(user_name,password):
+    """
+    create an instance of user logins
+    """
+    new_user = User(user_name,password)
+    return new_user
 
+def save_user_logins(logins):
+    """
+    save user logins
+    """
+    logins.save_user_logins()
 
+def delete_logins(logins):
+    """
+    Function that deletes logins
+    """
+    logins.delete_user_logins()
