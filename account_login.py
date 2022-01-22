de11f7e6e1d7d5bbf4ce9619e46fef340a285b7e
ivymmurithi <ivymurithi@gmail.com>
2022-01-22 11:09:33 +0300

    initalialize object and test it. passed

--- account_login.py
+++ account_login.py
@@ -3,11 +3,6 @@ class User:
     A class that generates new instances of user accounts created
     """
 
-    user_logins = []
-    """
-    An empty list to store the user_name and password
-    """
-
     def __init__(self,user_name,password):
         """
         This method will create new instances of the User class.
@@ -19,3 +14,10 @@ class User:
 
         self.user_name = user_name
         self.password = password
+
+
+    user_logins = []
+    """
+    An empty list to store the user_name and password
+    """
+
