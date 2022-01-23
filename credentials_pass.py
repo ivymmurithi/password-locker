20fb5fc63afee3dacc800829457b02c5be2c9ed6
ivymmurithi <ivymurithi@gmail.com>
2022-01-23 13:50:56 +0300

    create credentials class initialization and empty list to store data

--- credentials_pass.py
+++ credentials_pass.py
@@ -1,4 +1,11 @@
 class Credentials:
     """
-    Class that generates new instances of contacts
+    Class that generates new instances of credentials
     """
+
+    def __init__(self,social_username,social_password):
+        
+        self.social_username = social_username
+        self.social_password = social_password
+
+        socials_list = []
