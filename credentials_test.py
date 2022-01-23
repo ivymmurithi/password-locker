ce876e3b470550b5acd41009df9610ea208904f2
ivymmurithi <ivymurithi@gmail.com>
2022-01-23 14:04:08 +0300

    test initialization, passed

--- credentials_test.py
+++ credentials_test.py
@@ -1,3 +1,18 @@
 import unittest
 
 from credentials_pass import Credentials
+
+class TestCredentials(unittest.TestCase):
+
+    def setUp(self):
+
+        self.new_credentials = Credentials("FeuerMuschi","12345")
+
+    def test_init(self):
+        self.assertEqual(self.new_credentials.social_username,"FeuerMuschi")
+        self.assertEqual(self.new_credentials.social_password,"12345")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
