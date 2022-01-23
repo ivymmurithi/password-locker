2427fe30acdf625f659a6fa4362cb8cb303d12c4
ivymmurithi <ivymurithi@gmail.com>
2022-01-23 14:09:13 +0300

    add save method and test it

--- credentials_test.py
+++ credentials_test.py
@@ -8,10 +8,16 @@ class TestCredentials(unittest.TestCase):
 
         self.new_credentials = Credentials("FeuerMuschi","12345")
 
+    def tearDown(self):
+        Credentials.socials_list = []
+
     def test_init(self):
         self.assertEqual(self.new_credentials.social_username,"FeuerMuschi")
         self.assertEqual(self.new_credentials.social_password,"12345")
 
+    def test_save_socials(self):
+        self.new_credentials.save_socials()
+        self.assertEqual(len(Credentials.socials_list),1)
 
 
 if __name__ == "__main__":
